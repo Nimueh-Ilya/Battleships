4b4cc9b14dc5aa925a2c64b655b15b129d54f395
Nimueh-Ilya <139075582+Nimueh-Ilya@users.noreply.github.com>
2024-03-18 17:31:01 +0000

    Add Sunken ship checker and hits methods

--- src/ship.ts
+++ src/ship.ts
@@ -3,12 +3,23 @@ export class Ship {
   hits: number;
   sunk: boolean;
 
-  constructor(length: number, hits: number, sunk: boolean = false) {
+  constructor(length: number, hits: number = 0, sunk: boolean = false) {
     this.length = length;
     this.hits = hits;
     this.sunk = sunk;
   }
+  hit() {
+    if (this.hits < this.length) {
+      this.hits++;
+    }
+  }
 
+  isSunck() {
+    if (this.hits === this.length) {
+      this.sink();
+    }
+    return;
+  }
   sink() {
     this.sunk = true;
   }
