ff46fde28552130dab3df07c3fc897edf6d3b7b4
Nimueh-Ilya <139075582+Nimueh-Ilya@users.noreply.github.com>
2024-03-19 20:55:50 +0000

    Start placeship method

--- src/gameboard.ts
+++ src/gameboard.ts
@@ -1,3 +1,5 @@
+import { Ship } from "./ship";
+
 type gameboardgrid = (number | string)[][];
 export class Gameboard {
   attacks: number;
@@ -6,5 +8,10 @@ export class Gameboard {
   constructor(attacks: number = 0, board: gameboardgrid) {
     this.attacks = attacks;
     this.board = board;
+  }
+  placeship(positon: "horizontal" | "vertical", xpos: number, ypos: number) {
+    if (positon === "horizontal") {
+    } else {
+    }
   }
 }
