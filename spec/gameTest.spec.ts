2e58a06a4c270cef65e89c733ba4ae36d75e55d2
Nimueh-Ilya <139075582+Nimueh-Ilya@users.noreply.github.com>
2024-05-14 14:18:14 +0100

    Start writing tests for game.ts and players.ts I trolled earlier that is not how to test code, ill fix it in the upcoming commits

---
+++ spec/gameTest.spec.ts
@@ -0,0 +1,31 @@
+import { game } from "../src/game";
+
+test("check Win PLAYER1 WINS", () => {
+  const gameOne = new game();
+  gameOne.player1.sunkenShips.push(
+    gameOne.player2Battleship,
+    gameOne.player2Carrier,
+    gameOne.player2Destroyer,
+    gameOne.player2PatrolBoat,
+    gameOne.player2Submarine
+  );
+  gameOne.checkWin(gameOne.player1);
+  expect(gameOne.gameStatus).toBe("PLAYER1 WIN");
+});
+test("check Win PLAYER2 WINS", () => {
+  const gameOne = new game();
+  gameOne.player2.sunkenShips.push(
+    gameOne.player1Battleship,
+    gameOne.player1Carrier,
+    gameOne.player1Destroyer,
+    gameOne.player1PatrolBoat,
+    gameOne.player1Submarine
+  );
+  gameOne.checkWin(gameOne.player2);
+  expect(gameOne.gameStatus).toBe("PLAYER2 WIN");
+});
+
+test("check Win PLAYER1 WINS", () => {
+  const gameOne = new game();
+  expect(gameOne.gameStatus).toBe("");
+});
