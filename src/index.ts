d4241486e91605f75fa4d8397254be78b1de69cd
Nimueh-Ilya <139075582+Nimueh-Ilya@users.noreply.github.com>
2024-05-16 23:35:45 +0100

    Attempt at simulating the game IDK how to this wihtout user input

--- src/index.ts
+++ src/index.ts
@@ -10,4 +10,19 @@ import {
 import { game } from "./game";
 const gameEngine = () => {
   const gameOne = new game();
+  const player1PlaceShips = () => {
+    gameOne.player1.gameboard.placeShip(gameOne.player1.shipBattleship, 0, 0);
+    gameOne.player1.gameboard.placeShip(gameOne.player1.shipCarrier, 1, 0);
+    gameOne.player1.gameboard.placeShip(gameOne.player1.shipDestroyer, 2, 0);
+    gameOne.player1.gameboard.placeShip(gameOne.player1.shipPatrolBoat, 3, 0);
+    gameOne.player1.gameboard.placeShip(gameOne.player1.shipSubmarine, 4, 0);
+  };
+  const player2PlaceShips = () => {
+    gameOne.player2.gameboard.placeShip(gameOne.player2.shipBattleship, 0, 0);
+    gameOne.player2.gameboard.placeShip(gameOne.player2.shipCarrier, 1, 0);
+    gameOne.player2.gameboard.placeShip(gameOne.player2.shipDestroyer, 2, 0);
+    gameOne.player2.gameboard.placeShip(gameOne.player2.shipPatrolBoat, 3, 0);
+    gameOne.player2.gameboard.placeShip(gameOne.player2.shipSubmarine, 4, 0);
+  };
+  while (gameOne.gameStatus === "") {}
 };
