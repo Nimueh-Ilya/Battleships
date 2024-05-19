5ce461159915e16f8bda02cad0177e3d3ec1961f
Nimueh-Ilya <139075582+Nimueh-Ilya@users.noreply.github.com>
2024-05-19 18:08:36 +0100

    Fix game test unit

--- spec/gameTest.spec.ts
+++ spec/gameTest.spec.ts
@@ -1,36 +1,36 @@
-import { game } from "../src/game";
+import { Game } from "../src/game";
 
 test("check Win PLAYER1 WINS", () => {
-  const gameOne = new game();
+  const gameOne = new Game();
   gameOne.player1.sunkenShips.push(
-    gameOne.player2Battleship,
-    gameOne.player2Carrier,
-    gameOne.player2Destroyer,
-    gameOne.player2PatrolBoat,
-    gameOne.player2Submarine
+    gameOne.player2.ships.Battleship,
+    gameOne.player2.ships.Carrier,
+    gameOne.player2.ships.Destroyer,
+    gameOne.player2.ships.PatrolBoat,
+    gameOne.player2.ships.Submarine
   );
   gameOne.checkWin(gameOne.player1);
   expect(gameOne.gameStatus).toBe("PLAYER1 WIN");
 });
 test("check Win PLAYER2 WINS", () => {
-  const gameOne = new game();
+  const gameOne = new Game();
   gameOne.player2.sunkenShips.push(
-    gameOne.player1Battleship,
-    gameOne.player1Carrier,
-    gameOne.player1Destroyer,
-    gameOne.player1PatrolBoat,
-    gameOne.player1Submarine
+    gameOne.player1.ships.Battleship,
+    gameOne.player1.ships.Carrier,
+    gameOne.player1.ships.Destroyer,
+    gameOne.player1.ships.PatrolBoat,
+    gameOne.player1.ships.Submarine
   );
   gameOne.checkWin(gameOne.player2);
   expect(gameOne.gameStatus).toBe("PLAYER2 WIN");
 });
 
 test("check Win PLAYER1 WINS", () => {
-  const gameOne = new game();
+  const gameOne = new Game();
   expect(gameOne.gameStatus).toBe("");
 });
 test("Switch game turn", () => {
-  const gameOne = new game();
+  const gameOne = new Game();
   expect(gameOne.gameTurn).toBe("");
   gameOne.changeTurn();
   expect(gameOne.gameTurn).toBe("Player 2");
