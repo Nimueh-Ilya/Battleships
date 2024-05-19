159e48d19472458b6feefe2a70bc8f66684b198f
Nimueh-Ilya <139075582+Nimueh-Ilya@users.noreply.github.com>
2024-05-19 18:09:56 +0100

    Remove comments

--- src/player.ts
+++ src/player.ts
@@ -10,26 +10,12 @@ import {
 
 export class Player {
   gameboard: Gameboard;
-  // shipDestroyer: Destroyer;
-  // shipCarrier: Carrier;
-  // shipBattleship: Battleship;
-  // shipSubmarine: Submarine;
-  // shipPatrolBoat: patrolBoat;
   sunkenShips: Ship[];
   ships: { [key: string]: Ship };
 
-  constructor() // shipDestroyer: Destroyer,
-  // shipCarrier: Carrier,
-  // shipBattleship: Battleship,
-  // shipSubmarine: Submarine,
-  // shipPatrolBoat: patrolBoat
-  {
+  constructor() {
     this.gameboard = new Gameboard();
-    // this.shipDestroyer = shipDestroyer;
-    // this.shipCarrier = shipCarrier;
-    // this.shipBattleship = shipBattleship;
-    // this.shipSubmarine = shipSubmarine;
-    // this.shipPatrolBoat = shipPatrolBoat;
+
     this.ships = {
       destroyer: new Destroyer(),
       carrier: new Carrier(),
